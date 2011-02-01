1181a4f959cec91fd539fe5c9aefca620826807b
Louis-Alban KIM <louis.alban.kim@virtuality2reality.com>
2011-02-01 12:47:23 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DepotRado::Application.config.secret_token = 'b00f8507fd01835a81659b540a8d178dfc7fd5173ecc6b2a07086412e8231832cb682333fa37596fa1b3c94991ed1612a15983b00515c9fb88533bff89a1e67e'
