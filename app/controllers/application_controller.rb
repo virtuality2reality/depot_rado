11218181ff2ec3ac2fb732bd8981e2a0c0920e6f
Rakotobe <rakotobe@rakotobe.(none)>
2011-02-08 15:22:54 +0300

    Task I - Playtime

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
   
   protected
     def authorize
-      unless User.find_by_id(session[:user_id])
+      unless User.find_by_id(session[:user_id]) || session[:user_id] == 0
         redirect_to login_url, :notice => "Please log in"
       end
     end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,12 +5,18 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if user = User.authenticate(params[:name], params[:password])
-      session[:user_id] = user.id
+    if User.count.zero?
+      session[:user_id] = 0
       redirect_to admin_url
     else
-      redirect_to login_url, :alert => "Invalid user/password combination"
+      if user = User.authenticate(params[:name], params[:password])
+        session[:user_id] = user.id
+        redirect_to admin_url
+      else
+        redirect_to login_url, :alert => "Invalid user/password combination"
+      end
     end
+    
   end
 
   def destroy
